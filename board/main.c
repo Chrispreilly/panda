abda8d05b85510d2958387fd5a628fbec194963f
Chrispreilly <cpreilly@buffalo.edu>
2020-11-12 22:06:26 -0500

    enable safety to default subaru

--- board/main.c
+++ board/main.c
@@ -106,13 +106,13 @@ void debug_ring_callback(uart_ring *ring) {
 
 // ****************************** safety mode ******************************
 
-// this is the only way to leave silent mode
+// this is the only way to leave mode
 void set_safety_mode(uint16_t mode, int16_t param) {
   uint16_t mode_copy = mode;
   int err = set_safety_hooks(mode_copy, param);
   if (err == -1) {
     puts("Error: safety set mode failed. Falling back to SILENT\n");
-    mode_copy = SAFETY_SILENT;
+    mode_copy = SAFETY_SUBARU;
     err = set_safety_hooks(mode_copy, 0);
     if (err == -1) {
       puts("Error: Failed setting SILENT mode. Hanging\n");
@@ -834,7 +834,7 @@ int main(void) {
   // use TIM2->CNT to read
 
   // init to SILENT and can silent
-  set_safety_mode(SAFETY_SILENT, 0);
+  set_safety_mode(SAFETY_SUBARU, 0);
 
   // enable CAN TXs
   current_board->enable_can_transceivers(true);
